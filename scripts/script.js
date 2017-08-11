6ff8d08191622d7d123defec7742030592bf898b
snovey <isnovey@gmail.com>
2017-08-12 01:15:16 +0800

    release beta version

---
+++ scripts/script.js
@@ -0,0 +1,28 @@
+hexo.extend.generator.register('single-page', function(locals) {
+  var config = this.config;
+  var singlePageConfig = config.single_page;
+  var results = [];
+
+  singlePageConfig.forEach(function(v,k) {
+    results = results.concat({
+      path: v  + "/index.html",
+      layout: ['page']
+    });
+  });
+  return results;
+});
+
+hexo.extend.filter.register('after_post_render', function(data){
+  // console.log('---' + data.title + '---');
+  // if (!data.title) {
+  //   console.log(data)
+  // }
+  try {
+    // data.content.match(/<img([^>]*)\ssrc=/gi).forEach(x => console.log('+++' + x))
+    data.content = data.content.replace(/<img([^>]*)\ssrc=/gi, '<img$1 class="lazyload"  data-src=');
+    // data.content.match(/<img([^>]*)\sdata-src=/gi).forEach(x => console.log('+++' + x))
+    return data;
+  } catch (e) {
+    return data;
+  }
+});
